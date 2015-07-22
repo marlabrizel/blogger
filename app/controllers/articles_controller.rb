1cea1420f45b509619e12e5148c5d0a6aeedf89a
Marla Brizel <marlabrizel@gmail.com>
2015-07-22 15:08:57 -0600

    add flash notices

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    flash.notice "Article '#{@article.title} Created!'"
     redirect_to @article
   end
 
@@ -25,12 +26,14 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' Updated!"
     redirect_to @article
   end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+    flash.notice = "Article Deleted!"
     redirect_to articles_path
   end
 end
